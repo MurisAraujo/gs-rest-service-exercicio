a0b1c8c17ddcc60bd7c730ad95398db9cac8c80f
MURILO AMURIM DE ARAUJO <murilo.aaraujo@CAS474749C148A.local>
2023-03-15 22:35:10 -0300

    adicionando comentarios

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -34,12 +34,15 @@ public class GreetingControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	
 	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
 				.andExpect(jsonPath("$.content").value("Hello, World!"));
 	}
+
+	//
 
 	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
